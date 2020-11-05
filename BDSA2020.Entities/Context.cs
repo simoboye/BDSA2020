36b94b7a3c0a68e379bfa28179c8fd89a38e5258
simoboye <simonbj@live.dk>
2020-11-05 17:31:51 +0100

    Data in the Saved table

--- BDSA2020.Entities/Context.cs
+++ BDSA2020.Entities/Context.cs
@@ -59,7 +59,7 @@ namespace BDSA2020.Entities
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
-            modelBuilder.Entity<Saved>().HasKey(e => new { e.StudentId, e.PlacementDescriptionId });
+            modelBuilder.Entity<Saved>().HasKey(s => new { s.StudentId, s.PlacementDescriptionId });
 
             ParseEnums(modelBuilder);
 
@@ -68,48 +68,12 @@ namespace BDSA2020.Entities
                 .HasMany(e => e.PlacementDescriptions)
                 .WithOne(e => e.Company);
 
-            var company = new Company
-            {
-                Id = 1,
-                Name = "UML-central"
-            };
+            var company = new Company { Id = 1, Name = "UML-central" };
 
             var descriptions = new[]
             {
-                new PlacementDescription
-                {
-                    Id = 1,
-                    Degree = Degree.Other,
-                    Keywords = Keywords.UML,
-                    MinSalary = 10,
-                    MinWorkingHours = 1,
-                    MaxWorkingHours = 100,
-                    Agreement = false,
-                    Location = "Copenhagen",
-                    LastApplyDate = new DateTime(2020, 12, 3),
-                    Email = "ApplyHere@apply.com",
-                    Thumbnail = new Uri("https://starwarsblog.starwars.com/wp-content/uploads/2020/04/best-friend-in-galaxy-chewbacca_TALL.jpg"),
-                    Title = "UML designer",
-                    Description = "You should be able to do UML diagrams correctly",
-                    CompanyId = company.Id
-                },
-                new PlacementDescription
-                {
-                    Id = 2,
-                    Degree = Degree.Bachelor,
-                    Keywords = Keywords.CSharp,
-                    MinSalary = 100,
-                    MinWorkingHours = 10,
-                    MaxWorkingHours = 100,
-                    Agreement = true,
-                    Location = "Copenhagen",
-                    LastApplyDate = new DateTime(2020, 12, 10),
-                    Email = "ApplyHere@apply.com",
-                    Thumbnail = new Uri("https://starwarsblog.starwars.com/wp-content/uploads/2020/04/best-friend-in-galaxy-chewbacca_TALL.jpg"),
-                    Title = "C# developer",
-                    Description = "Join our team in of skilled developers",
-                    CompanyId = company.Id
-                }
+                new PlacementDescription { Id = 1, Degree = Degree.Other, Keywords = Keywords.UML, MinSalary = 10, MinWorkingHours = 1, MaxWorkingHours = 100, Agreement = false, Location = "Copenhagen", LastApplyDate = new DateTime(2020, 12, 3), Email = "ApplyHere@apply.com", Thumbnail = new Uri("https://starwarsblog.starwars.com/wp-content/uploads/2020/04/best-friend-in-galaxy-chewbacca_TALL.jpg"), Title = "UML designer", Description = "You should be able to do UML diagrams correctly", CompanyId = company.Id },
+                new PlacementDescription { Id = 2, Degree = Degree.Bachelor, Keywords = Keywords.CSharp, MinSalary = 100, MinWorkingHours = 10, MaxWorkingHours = 100, Agreement = true, Location = "Copenhagen", LastApplyDate = new DateTime(2020, 12, 10), Email = "ApplyHere@apply.com", Thumbnail = new Uri("https://starwarsblog.starwars.com/wp-content/uploads/2020/04/best-friend-in-galaxy-chewbacca_TALL.jpg"), Title = "C# developer", Description = "Join our team in of skilled developers", CompanyId = company.Id }
             };
 
             modelBuilder
@@ -127,50 +91,18 @@ namespace BDSA2020.Entities
             modelBuilder
                 .Entity<Student>()
                 .HasData(
-                    new Student
-                    {
-                        Id = 1,
-                        Degree = Degree.Bachelor,
-                        Keywords = Keywords.BackEnd,
-                        MinSalary = 100,
-                        MinWorkingHours = 5,
-                        MaxWorkingHours = 20,
-                        Agreement = false,
-                        Location = "Nowhere"
-                    },
-                    new Student
-                    {
-                        Id = 2,
-                        Degree = Degree.Master,
-                        Keywords = Keywords.FrontEnd,
-                        MinSalary = 1000,
-                        MinWorkingHours = 532,
-                        MaxWorkingHours = 43243,
-                        Agreement = false,
-                        Location = "Anywhere"
-                    },
-                    new Student
-                    {
-                        Id = 3,
-                        Degree = Degree.PhD,
-                        Keywords = Keywords.Testing,
-                        MinSalary = 10000,
-                        MinWorkingHours = 5000,
-                        MaxWorkingHours = 5001,
-                        Agreement = true,
-                        Location = "Glostrup"
-                    },
-                    new Student
-                    {
-                        Id = 4,
-                        Degree = Degree.Other,
-                        Keywords = Keywords.UML,
-                        MinSalary = 1,
-                        MinWorkingHours = 1,
-                        MaxWorkingHours = 5,
-                        Agreement = true,
-                        Location = "Italy"
-                    }
+                    new Student { Id = 1, Degree = Degree.Bachelor, Keywords = Keywords.BackEnd, MinSalary = 100, MinWorkingHours = 5, MaxWorkingHours = 20, Agreement = false, Location = "Nowhere" },
+                    new Student { Id = 2, Degree = Degree.Master, Keywords = Keywords.FrontEnd, MinSalary = 1000, MinWorkingHours = 532, MaxWorkingHours = 43243, Agreement = false, Location = "Anywhere" },
+                    new Student { Id = 3, Degree = Degree.PhD, Keywords = Keywords.Testing, MinSalary = 10000, MinWorkingHours = 5000, MaxWorkingHours = 5001, Agreement = true, Location = "Glostrup" },
+                    new Student { Id = 4, Degree = Degree.Other, Keywords = Keywords.UML, MinSalary = 1, MinWorkingHours = 1, MaxWorkingHours = 5, Agreement = true, Location = "Italy" }
+                );
+
+            modelBuilder
+                .Entity<Saved>()
+                .HasData(
+                    new Saved { StudentId = 1, PlacementDescriptionId = 1 },
+                    new Saved { StudentId = 1, PlacementDescriptionId = 2 },
+                    new Saved { StudentId = 2, PlacementDescriptionId = 1 }
                 );
         }
     }
--- BDSA2020.Entities/Saved.cs
+++ BDSA2020.Entities/Saved.cs
@@ -4,7 +4,6 @@ namespace BDSA2020.Entities
     {
         public int StudentId { get; set; }
         public virtual Student Student { get; set; }
-
         public int PlacementDescriptionId { get; set; }
         public PlacementDescription PlacementDescription { get; set; }
     }
