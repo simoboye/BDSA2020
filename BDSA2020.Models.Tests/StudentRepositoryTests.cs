30a0442636a85ba48a72a2a1c5614ccf73b8d944
simoboye <simonbj@live.dk>
2020-11-12 17:21:50 +0100

    Added delete, create and get

--- BDSA2020.Models.Tests/StudentRepositoryTests.cs
+++ BDSA2020.Models.Tests/StudentRepositoryTests.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Linq;
 using System.Threading.Tasks;
 using BDSA2020.Entities;
 using Microsoft.Data.Sqlite;
@@ -34,6 +35,78 @@ namespace BDSA2020.Models.Tests
 
             // Assert
             Assert.Equal(4, actual.Count);
+        }
+
+        [Fact]
+        public async Task GetStudent_returns_the_requested_student() 
+        {
+            var actual = await repository.GetStudentAsync(1);
+
+            var expected = new Student 
+            { 
+                Id = 1, 
+                Degree = Degree.Bachelor, 
+                MinSalary = 100, 
+                MinWorkingHours = 5, 
+                MaxWorkingHours = 20, 
+                Agreement = false, 
+                Location = "Nowhere" 
+            };
+
+            Assert.Equal(expected.Id, actual.Id);
+            Assert.Equal(expected.Degree, actual.Degree);
+        }
+
+        [Fact]
+        public async Task GetStudent_returns_ArgumentException_on_not_found_student() 
+        {
+            await Assert.ThrowsAsync<ArgumentException>(() => repository.GetStudentAsync(100));
+        }
+
+        [Fact]
+        public async Task CreateStudent_returns_the_id_of_created_student()
+        {
+            var student = new Student 
+            { 
+                Degree = Degree.Bachelor, 
+                MinSalary = 100, 
+                MinWorkingHours = 5, 
+                MaxWorkingHours = 20, 
+                Agreement = false, 
+                Location = "Nowhere" 
+            };
+
+            var studentsList = await context.Students.ToListAsync();
+            var expected = studentsList.OrderByDescending(s => s.Id)
+                                        .FirstOrDefault();
+
+            var actual = await repository.CreateStudentAsync(student);
+
+            Assert.Equal(expected.Id + 1, actual);
+        }
+
+        [Fact]
+        public async Task CreateStudent_returns_ArguementException_on_conflict()
+        {
+            var student = new Student 
+            { 
+                Id = 1,
+                Degree = Degree.Bachelor, 
+                MinSalary = 100, 
+                MinWorkingHours = 5, 
+                MaxWorkingHours = 20, 
+                Agreement = false, 
+                Location = "Nowhere" 
+            };
+            await Assert.ThrowsAsync<ArgumentException>(() => repository.CreateStudentAsync(student));
+        }
+
+        [Fact]
+        public async Task DeleteStudent_returns_true() 
+        {
+            var actual = await repository.DeleteStudentAsync(1);
+
+            Assert.True(actual);
         }
     }
 }
--- BDSA2020.Models/StudentRepository.cs
+++ BDSA2020.Models/StudentRepository.cs
@@ -27,5 +27,44 @@ namespace BDSA2020.Models
             return await students.ToListAsync();
 
         }
+
+        public async Task<Student> GetStudentAsync(int id)
+        {
+            var student = await context.Students.FindAsync(id);
+
+            if (student == null)
+            {
+                throw new ArgumentException("The student doesn't exist");
+            }
+
+            return student;
+
+        }
+
+        public async Task<int> CreateStudentAsync(Student student)
+        {
+            var checkIfConflict = await context.Students.FindAsync(student.Id);
+
+            if (checkIfConflict != null)
+            {
+                throw new ArgumentException("The student already exists");
+            }
+
+            await context.Students.AddAsync(student);
+            await context.SaveChangesAsync();
+
+            return student.Id;
+        }
+
+        public async Task<bool> DeleteStudentAsync(int id)
+        {
+            var student = await context.Students.FindAsync(id);
+
+            context.Students.Remove(student);
+
+            await context.SaveChangesAsync();
+
+            return true;
+        }
     }
 }
