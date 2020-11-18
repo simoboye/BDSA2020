71ef2aba0155c664a8d92263e711a0c0ceac219f
simoboye <simonbj@live.dk>
2020-11-18 12:58:02 +0100

    Added Shared classlib, to contain Util class

---
+++ BDSA2020.Shared/Util.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace BDSA2020.Shared
+{
+    public class Util
+    {
+        public static void LogError(Exception e)
+        {   
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine(e); // Primitive logging
+            Console.ResetColor();
+        }
+    }
+}
