456eed4255934f0141600cb738ac687b67b8148e
Bjørnar Haugstad Jåtten <bjja@itu.dk>
2020-12-06 00:13:55 +0100

    Added userfeedback for saving placment description

--- BDSA2020.View/wwwroot/main.js
+++ BDSA2020.View/wwwroot/main.js
@@ -35,11 +35,19 @@ document.querySelector('#controls').addEventListener('click', (event) => {
   }
 });
 
-
 function checkMore(){
     //calculates if there is more desriptions from scroll values (455 is height of one description (px))
     if((document.getElementById('scroll-container').scrollHeight - 455.0) == document.getElementById('scroll-container').scrollTop){
       console.log("NO MORE DESCRIPTION")
       //TODO add loadMore() logic here
     }
-  }
+}
+
+function showAlert(){
+  document.getElementById('savedAlert').hidden = false;
+  setTimeout(function(){ hideAlert() }, 3000);
+}
+
+function hideAlert(){
+  document.getElementById('savedAlert').hidden = true;
+}
