e9effb760b4bf09005279e316e54950b15b392b8
fwen <fwen@itu.dk>
2020-12-06 13:23:27 +0100

    removed stuff

--- BDSA2020.View/wwwroot/main.js
+++ BDSA2020.View/wwwroot/main.js
@@ -1,11 +1,3 @@
-
-window.CardAnimation = () => {
-  //use this for future JS where Blazor needs to be loaded
-  //document.getElementById('scroll-container').onscroll = function() {
-  //    checkMore();
-  //};
-}
-
 function goPrev() {
   document.getElementById('scroll-container').scrollBy({ 
     left: -40,
@@ -32,14 +24,3 @@ document.querySelector('#controls').addEventListener('click', (event) => {
     $slide.scrollIntoView();
   }
 });
-
-
-function checkMore(){
-    //calculates if there is more desriptions from scroll values (455 is height of one description (px))
-    console.log((document.getElementById('scroll-container').scrollLeft - document.getElementById('scroll-container').clientWidth));
-    console.log(document.getElementById('scroll-container').scrollWidth);
-    if((document.getElementById('scroll-container').scrollLeft - document.getElementById('scroll-container').clientWidth) == document.getElementById('scroll-container').scrollLeft){
-      console.log("NO MORE DESCRIPTION")
-      //TODO add loadMore() logic here
-    }
-  }
