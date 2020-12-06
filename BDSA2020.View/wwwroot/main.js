76fff124c17551c3129b34fd3eabd6da43e9f45d
frederik0312 <38206067+frederik0312@users.noreply.github.com>
2020-12-06 13:30:34 +0100

    Nav Buttons works now (#47)
    * Works now
    
    * removed stuff
    
    Co-authored-by: fwen <fwen@itu.dk>

--- BDSA2020.View/wwwroot/main.js
+++ BDSA2020.View/wwwroot/main.js
@@ -1,25 +1,15 @@
-
-window.CardAnimation = () => {
-  //use this for future JS where Blazor needs to be loaded
-  //document.getElementById('scroll-container').onscroll = function() {
-  //    checkMore();
-  //};
-}
-
 function goPrev() {
   document.getElementById('scroll-container').scrollBy({ 
-    top: -40,
+    left: -40,
     behavior: 'smooth' 
   });
 }
 
 function goNext() {
-
   document.getElementById('scroll-container').scrollBy({ 
-    top: 40,
+    left: 40,
     behavior: 'smooth' 
   });
-  checkMore();
 }
 
 document.querySelector('#controls').addEventListener('click', (event) => {
@@ -34,12 +24,3 @@ document.querySelector('#controls').addEventListener('click', (event) => {
     $slide.scrollIntoView();
   }
 });
-
-
-function checkMore(){
-    //calculates if there is more desriptions from scroll values (455 is height of one description (px))
-    if((document.getElementById('scroll-container').scrollHeight - 455.0) == document.getElementById('scroll-container').scrollTop){
-      console.log("NO MORE DESCRIPTION")
-      //TODO add loadMore() logic here
-    }
-  }
