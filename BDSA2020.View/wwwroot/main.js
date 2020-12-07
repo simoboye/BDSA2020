308e182fbfdaa4e80c7af06da7eac975d9d3a3be
Bjørnar Haugstad Jåtten <bjja@itu.dk>
2020-12-08 00:09:25 +0100

    Fixed bug in view

--- BDSA2020.View/wwwroot/main.js
+++ BDSA2020.View/wwwroot/main.js
@@ -12,19 +12,6 @@ function goNext() {
   });
 }
 
-document.querySelector('#controls').addEventListener('click', (event) => {
-  const $slide = document.querySelector(event.target.getAttribute('href'));
-  if (!$slide) return;
-  
-  if ($slide.scrollIntoViewIfNeeded) {
-    event.preventDefault();
-    $slide.scrollIntoViewIfNeeded();
-  } else if ($slide.scrollIntoView) {
-    event.preventDefault();
-    $slide.scrollIntoView();
-  }
-});
-
 function checkMore(){
     //calculates if there is more desriptions from scroll values (455 is height of one description (px))
     if((document.getElementById('scroll-container').scrollHeight - 455.0) == document.getElementById('scroll-container').scrollTop){
